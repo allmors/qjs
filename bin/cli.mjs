989ed604516b6bec097237e8cf41b6166c50669a
allmors <jesmora@protonmail.com>
2024-09-30 02:50:57 +0000

    update

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -3,14 +3,18 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import open from 'open';
+// import { fileURLToPath } from 'url';
+
 /**
  * @description start server
  */
 import { start, file } from "../src/index.mjs";
 
 const command = process.argv[2];
-
 process.env.QJS_ROOTDIR = process.env.QJS_ROOTDIR || 'functions';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const template = `// @see https://github.com/allmors/qjs/
 import qjs from 'fast-qjs/core';
@@ -34,6 +38,37 @@ export default async function (params, ctx) {
 }
 `;
 
+// function copyDirectory(sourceDir, targetDir) {
+//     // 如果目标目录不存在或为空，才进行复制
+//     if (!fs.existsSync(targetDir) || fs.readdirSync(targetDir).length === 0) {
+//         if (!fs.existsSync(targetDir)) {
+//             fs.mkdirSync(targetDir, { recursive: true });
+//         }
+
+//         const items = fs.readdirSync(sourceDir);
+
+//         for (const item of items) {
+//             const sourceItemPath = path.join(sourceDir, item);
+//             const targetItemPath = path.join(targetDir, item);
+
+//             const stats = fs.statSync(sourceItemPath);
+
+//             if (stats.isDirectory()) {
+//                 copyDirectory(sourceItemPath, targetItemPath);
+//             } else {
+//                 fs.copyFileSync(sourceItemPath, targetItemPath);
+//             }
+//         }
+//     } else {
+//         console.info('Target directory already exists and is not empty. Skipping copy.');
+//     }
+// }
+
+// const aProjectPluginsPath = path.resolve(__dirname, '../plugins');
+// const bProjectPluginsPath = path.resolve(process.cwd(), 'plugins');
+
+// copyDirectory(aProjectPluginsPath, bProjectPluginsPath);
+
 if (command === '--init') {
     if (!fs.existsSync(process.env.QJS_ROOTDIR)) {
         fs.mkdirSync(process.env.QJS_ROOTDIR);
@@ -44,6 +79,6 @@ if (command === '--init') {
 
 await start();
 
-if (fs.existsSync(file('qjs.js')) || fs.existsSync(file('qjs.mjs'))) {
+if ((fs.existsSync(file('qjs.js')) || fs.existsSync(file('qjs.mjs'))) && command === '--open') {
     open(`http://localhost:${process.env.QJS_PORT}${process.env.QJS_PREFIX}qjs`);
 }
